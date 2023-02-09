87713776876968d81d9e497433d9443a2ea5184b
Asifking0786 <122234588+Asifking0786@users.noreply.github.com>
2023-02-09 13:31:22 +0500

    javascript-work3

---
+++ app.js
@@ -0,0 +1,202 @@
+        // Assignment 03
+    // Chapter no 05
+
+    document.write('<h1> "Chapter no 05 " </h1>')
+
+    document.write('<h2> "Assignment 03 " </h2>')
+
+    // Question no 01
+
+    document.write('<h3> "Example Ques. # 01 " </h3>')
+    
+    document.write("<h3>Addition:</h3>")
+    var a= (3)
+    var b= (5)
+    var c= a + b
+    document.write("Sum of" + " " + a + " " + "and" +" " + b + " "+ "is" + " " + c )
+
+
+    // Question No 02 (A)
+
+    document.write('<h3> "Example Ques. # 02 A " </h3>')
+
+    document.write("<h3>Multipication:</h3>")
+    var ma=(3)
+    var mb=(5)
+    var mc= ma * mb
+    document.write("Multiply of" + " " + ma + " " +  "and" + " " + mb + " " + "is" + " " + mc )
+
+    // Question No 02 (B)
+
+    document.write('<h3> "Example Ques. # 02 B" </h3>')
+
+    document.write("<h3>Division:</h3>")
+    var da=(5)
+    var db=(3)
+    var dc= ma / mb
+    document.write("Divide of" + " " + da + " " +  "and" + " " + db + " " + "is" + " " + dc )
+ 
+     // Question No 02 (C)
+
+     document.write('<h3> "Example Ques. # 02 C " </h3>')
+
+    document.write("<h3>Modulus:</h3>")
+    var moa=(5)
+    var mob=(3)
+    var moc= moa % mob
+    document.write("Divide of" + " " + moa + " " +  "and" + " " + mob + " " + "is" + " " + moc + "<br>")
+ 
+
+    // Question No 03 
+
+    document.write('<h3> "Example Ques. # 03 A " </h3>')
+   
+    var num=
+    document.write( "Value after variable declaration is" + " " + num)
+
+
+
+    document.write('<h3> "Example Ques. # 03 B " </h3>')
+    var num=(5)
+    document.write( "Intial value:" + num)
+
+
+
+    document.write('<h3> "Example Ques. # 03 C " </h3>')
+    var num=(5)
+    document.write("Value after increment is:" + " " + ++ num)
+
+
+
+    document.write('<h3> "Example Ques. # 03 D " </h3>')
+    var num=(4)
+    document.write("Value after increment is:" + " " + ++ num)
+
+
+
+    document.write('<h3> "Example Ques. # 03 E(a) " </h3>')
+    var numa=(5)
+    var numb=(8)
+    var numc = numa + 8
+    document.write( "Value after 2 Variable Addtion is:" + " " + numc)
+
+
+
+    document.write('<h3> "Example Ques. # 03 E(b) " </h3>')
+    var num=(4)
+    var numa=(5)
+    var numc = num + numa
+    document.write("Value after increment is:" + " " + ++ numc)
+
+
+
+    document.write('<h3> "Example Ques. # 03 F " </h3>')
+    var num=(5)
+    var numa = ++num
+    document.write("Value after increment is:" + " " + numa)
+
+
+
+    document.write('<h3> "Example Ques. # 03 G " </h3>')
+    var num=(6)
+    var numa = num
+    document.write("Value 6+1 after increment is:" + " " + ++num)
+
+
+
+    document.write('<h3> "Example Ques. # 03 H " </h3>')
+    var a= (6)
+    var b= (6)
+    var c= a+b 
+    document.write("Value after increment is 6+6 =" + ++c )
+
+
+
+    document.write('<h3> "Example Ques. # 03 I " </h3>')
+    var num=(6)
+    var numa = --num
+    document.write("Decrement the variable. 6-1 = " + numa)
+
+
+
+    document.write('<h3> "Example Ques. # 03 J" </h3>')
+    var a= (19)
+    var b= (6)
+    var c= a-b 
+    document.write("Value after Decrement is 19-6 =" + --c )
+
+
+
+    document.write('<h3> "Example Ques. # 03 K" </h3>')
+    var num=(10)
+    var numa = num / 3
+    document.write("After 10 dividing the variable's value by 3:" + " " + "=" + " " + numa)
+
+
+
+    document.write('<h3> "Example Ques. # 03 L" </h3>')
+    var a=(12)
+    var b=(3)
+    var c= a%b
+
+    document.write("After 12 Modulus the variable's value by 3:"  + " " + "=" + " " + c)
+
+    // Question No 04
+
+    document.write('<h3> "Ques. # 04 " </h3>')
+
+    document.write("<h5>Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie.?</h5>")
+  
+
+    document.write('<h3> "Example Ques. # 04 " </h3>')
+    var ticket =(600)
+    var buying =(5)
+    var total = ticket * buying
+    document.write("Total cost to buy 5 tickets to a movie is " + " " + total + " " + "PKR.")
+
+
+    // Question No 05
+
+    document.write('<h3> "Explain. # 05 " </h3>')
+    document.write('<h3> "Table of 3" </h3>')
+
+    var aa = 1
+    var ab = 2
+    var ac = 3
+    var ad = 4
+    var ae = 5
+    var af = 6
+    var ag = 7
+    var ah = 8
+    var ai = 9
+    var aj = 10
+
+    var ta = ac * aa  
+    document.write("3x1=" + ta + "<br>")
+
+    var tb = ac * ab
+    document.write("3x2=" + tb + "<br>")
+
+    var tc = ac * ac 
+    document.write("3x3=" + tc + "<br>")
+
+    var td = ac * ad
+    document.write("3x4=" + td + "<br>")
+
+    var te =  ac * ae
+    document.write("3x5=" + te + "<br>")
+
+    var tf = ac * af
+    document.write("3x6=" + tf + "<br>")
+
+    var tg = ac * ag
+    document.write("3x7=" + tg + "<br>")
+
+    var th = ac * ah
+    document.write("3x8=" + th + "<br>")
+    
+    var ti = ac * ai
+    document.write("3x9=" + ti + "<br>")
+
+    var tj = ac * aj
+    document.write("3x10=" + tj + "<br>")
